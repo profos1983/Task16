566e3b8080db4ad06f83e1d15e391749efb6ae8a
profos1983 <jianko@hotmail.com>
2026-01-14 13:19:06 +0300

    2026-01-14 часть 1

---
+++ Task16_3_1.Tests(NUnit)/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using Task16_3_1;
+namespace Task16_3_1.Tests_NUnit_
+{
+    public class Tests
+    {
+
+        [Test]
+        public void FindAll_Must_Return_Null()
+        {
+//Выполните тестирование функции FindAll при помощи moq - объекта.Используйте 
+//следующие три имени пользователей для добавления в moq-коллекцию: Антон, Иван, Алексей.
+//При написании теста используйте фреймворк NUnit. Для проверки наличия элемента в коллекции 
+//используйте метод That класса Assert и метод Any синтаксиса Linq.
+            var mockUserRepository = new Mock<IUserRepository> ();
+            mockUserRepository.Setup (x => x.
+            Assert.Pass();
+        }
+    }
+}
---
+++ Task16_3_1/Classes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task16_3_1
+{
+    public class UserRepository : IUserRepository
+    {
+        public IEnumerable<User> FindAll()
+        {
+            return null;
+        }
+    }
+
+    public interface IUserRepository
+    {
+        IEnumerable<User> FindAll();
+    }
+
+    public class User
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
